649df6e357e24151c85602b426f1ab92180bd834
Keyno <kofujiw@gmail.com>
2021-11-22 23:28:30 +0900

    add todo comment, for service function

--- src/main/scala/jp/co/keyno/sandbox/sample/domain/issue/IssueService.scala
+++ src/main/scala/jp/co/keyno/sandbox/sample/domain/issue/IssueService.scala
@@ -2,6 +2,7 @@ package jp.co.keyno.sandbox.sample.domain.issue
 
 class IssueService {
 
+  // TODO: fetch from repository
   def getIssueList: List[Issue] = {
     (1 to 3)
       .map(i => Issue(i, s"概要$i", s"説明$i"))
