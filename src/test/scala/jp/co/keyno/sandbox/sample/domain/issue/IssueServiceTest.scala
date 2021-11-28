37f045c53d9991135177e0c207f2b33e924b3dcb
Keyno <kofujiw@gmail.com>
2021-11-28 15:27:58 +0900

    IssueService の Test を Mockito を使ったテストも追加

--- src/test/scala/jp/co/keyno/sandbox/sample/domain/issue/IssueServiceTest.scala
+++ src/test/scala/jp/co/keyno/sandbox/sample/domain/issue/IssueServiceTest.scala
@@ -1,17 +1,36 @@
 package jp.co.keyno.sandbox.sample.domain.issue
 
+import jp.co.keyno.sandbox.sample.domain.repository.IssueRepository
 import munit.FunSuite
+import org.mockito.Mockito.{ doReturn, mock }
 import org.mockito.MockitoSugar
 
 class IssueServiceTest extends FunSuite with MockitoSugar {
-  private val mock = mock[IssueService]
+  private val mock = mock[IssueRepository]
 
   test("first munit test") {
     val expected = List(Issue(1, "タイトル1", "概要1"))
     doReturn(expected)
       .when(mock)
-      .getIssueList
+      .findAll()
 
-    assertEquals(mock.getIssueList, expected)
+    val service = new IssueService(mock)
+
+    assertEquals(service.getIssueList, expected)
+  }
+}
+
+class IssueServiceUsedMockitoTest extends FunSuite {
+  private val mockIssueService = mock(classOf[IssueRepository])
+
+  test("IssueService Test used Mockito") {
+    val expected = List(Issue(1, "タイトル1", "概要1"))
+    doReturn(expected)
+      .when(mockIssueService)
+      .findAll()
+
+    val service = new IssueService(mockIssueService)
+
+    assertEquals(service.getIssueList, expected)
   }
 }
